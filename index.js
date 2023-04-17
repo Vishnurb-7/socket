93a3c98e3f0afedbb14fcfcb744fb1515f612ce2
Vishnurb-7 <vishnurb4vishnu@gmail.com>
2023-04-17 16:45:57 +0530

    after some change

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ io.on("connection", (socket) => {
         // if user is not added previously
         if (!activeUsers.some((user) => user.userId === newUserId)) {
             activeUsers.push({ userId: newUserId, socketId: socket.id });
-            console.log("New User Connected", activeUsers);
+            // console.log("New User Connected", activeUsers);
         }
         // send all active users to new user
         io.emit("get-users", activeUsers);
@@ -31,8 +31,8 @@ io.on("connection", (socket) => {
     socket.on("send-message", (data) => {
         const { receiverId } = data;
         const user = activeUsers.find((user) => user.userId === receiverId);
-        console.log("Sending from socket to :", receiverId)
-        console.log("Data: ", data)
+        // console.log("Sending from socket to :", receiverId)
+        // console.log("Data: ", data)
         if (user) {
             io.to(user.socketId).emit("recieve-message", data);
         }
